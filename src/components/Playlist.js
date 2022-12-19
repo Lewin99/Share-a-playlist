18cd98b509dd2ba180394342d14324c96024bfc4
Lewin99 <l.ndahinyuk@alustudent.com>
2022-12-19 22:42:34 +0400

    9done

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,5 +1,7 @@
 import React,{useState} from 'react'
 import './Playlist.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function Playlist() {
     const[value,setvalue]=useState(null)
@@ -10,9 +12,9 @@ function Playlist() {
   return (
     <div className='input-div'>
     <input className='input' type='text' value={value} onChange={Handlevalue} placeholder="Enter a song name" />
-    <button>Add</button>
+    <Button >Add</Button>
     <div>
-     <button>Submit</button>
+     <Button variant='outline-danger'>Submit</Button>
     </div>
       </div>
   )
