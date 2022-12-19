4e37ccf7fcf0c2503db5fde3ce36b6ec79d08952
Lewin99 <l.ndahinyuk@alustudent.com>
2022-12-19 22:34:07 +0400

    done

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -3,9 +3,14 @@ import './Playlist.css'
 
 function Playlist() {
     const[value,setvalue]=useState(null)
+
+    const Handlevalue=(e)=>{
+     setvalue(e.target.value)
+     alert(value)
+    }
   return (
     <div className='input-div'>
-    <input className='input' type='text' value={value} placeholder="Enter a song name" />
+    <input className='input' type='text' value={value} onChange={Handlevalue} placeholder="Enter a song name" />
     <button>Add</button>
     <div>
      <button>Submit</button>
