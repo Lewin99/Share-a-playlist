3c8a77cb8bf1f2dc76919979734ecde9a3ba3da8
Lewin99 <l.ndahinyuk@alustudent.com>
2022-12-20 18:31:25 +0400

    done

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,20 +1,32 @@
 import React,{useState} from 'react'
 import './Playlist.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Playlistres from './Playlistres';
 
 function Playlist() {
     const[value,setvalue]=useState(null)
+    const[list,setitemlist]=useState([])
 
     const Handlevalue=(e)=>{
      setvalue(e.target.value)
     }
+
+    const handleadd=()=>{
+     setitemlist([...list,value])
+    }
   return (
     
     <div className='input-div'>
+        <div className='input-add'>
     <input className='input' type='text' value={value} onChange={Handlevalue} placeholder="Enter a song name" />
-    <button className='add-button'>Add</button>
+    <button className='add-button' onClick={handleadd}>Add</button>
+    <div className='added'>
+    <Playlistres items={list} />
+    </div>
+    
+    </div>
     <div className='submit-button-div'>
-     <button className='submit-button'>Submit</button>
+     <button className='submit-button' >Submit</button>
     </div>
       </div>
   )
---
+++ src/components/Playlistres.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './playlistres.css'
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { AiFillDelete } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+
+
+
+function Playlistres(props) {
+  return (
+    <div className='result'>{props.items.map(all=>{
+         return <div className='result-min'><p>{all}</p>
+         <div className='buttons'>
+        <div className='check'> <BsFillCheckCircleFill size="1.5em" color=" orangered" /></div>
+         <div className='delete'><AiFillDelete  size="1.5em"/></div>
+         <div className='update'><FiEdit size="1.5em"/></div>
+         
+         </div>
+            </div>
+    })
+    }</div>
+  )
+}
+
+export default Playlistres
